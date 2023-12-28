f624e15a486b9ac804109fb8f3dba7e313aa0568
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-12-29 00:00:57 +0530

    added get all users

--- api/main.go
+++ api/main.go
@@ -66,3 +66,26 @@ func jsonContentTypeMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func getUsers(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT * FROM users")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		users := []User{}
+		for rows.Next() {
+			var u User
+			if err := rows.Scan(&u.Id, &u.Name, &u.Email); err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, u)
+		}
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+		json.NewEncoder(w).Encode(users)
+	}
+}
